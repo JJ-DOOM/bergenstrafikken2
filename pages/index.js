fc3e9f81a0cd3b8bb70c63a4909ab7eee22fae1f
Erlend Stenberg <erlend.ste@gmail.com>
2020-12-03 14:19:45 +0100

    Minor tweaks to mobile view

--- pages/index.js
+++ pages/index.js
@@ -13,37 +13,22 @@ export default function Home({ trafficMessages }) {
       <Head>
         <title>Trafikkmeldinger i Bergen</title>
         <link rel="icon" href="/favicon.ico" />
-        <link
-            rel="preload"
-            href="/fonts/Source_Sans_Pro/SourceSansPro-Regular.ttf"
-            as="font"
-            crossOrigin=""
-          />
-          <link
-            rel="preload"
-            href="/fonts/Source_Sans_Pro/SourceSansPro-SemiBold.ttf"
-            as="font"
-            crossOrigin=""
-          />
-          <link
-            rel="preload"
-            href="/fonts/Source_Sans_Pro/SourceSansPro-Bold.ttf"
-            as="font"
-            crossOrigin=""
-          />
-        <meta name="description" content="Når BT forræder oss med abonnenttvang, snekrer vi selv" />
+        <link rel="preload" href="/fonts/Source_Sans_Pro/SourceSansPro-Regular.ttf" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/Source_Sans_Pro/SourceSansPro-SemiBold.ttf" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/Source_Sans_Pro/SourceSansPro-Bold.ttf" as="font" crossOrigin="" />
+        <meta name="description" content="Abonnementstvang --> ikke vær så vrang." />
         <meta property="og:url" content="https://bergenstrafikken.vercel.app" />
         <meta property="og:title" content="Trafikkmeldinger i Bergen - uten mellommannen" />
-        <meta property="og:description" content="Når BT forræder oss med abonnenttvang, snekrer vi selv" />
+        <meta property="og:description" content="Abonnementstvang --> ikke vær så vrang." />
         <meta property="og:image" content="/asd.png" />
       </Head>
 
       <main className={styles.main}>
         <div className={styles.banner}>(Nesten) alltid oppdatert</div>
         <div className={styles.icons}>
-            <Bus />
-            <Car />
-            <Train />
+          <Car />
+          <Bus className={styles.mobileHidden} />
+          <Train className={styles.mobileHidden} />
         </div>
         <h1 className={styles.title}>Lokale trafikkmeldinger</h1>
         <p className={styles.description}>levert på ekspertlig vis</p>
